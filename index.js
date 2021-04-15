a9c70215a9bfe526cd3a1242c48029ceb65cb327
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-15 09:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 function moveDodgerLeft() {
   let dodger = document.getElementById("dodger");
   document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
+  if (e.key === "ArrowLeft" ) {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
-
+    left > 0;
     dodger.style.left = `${left - 1}px`;
+    
   } else if (e.key === "ArrowRight") {
     dodger.style.left = `${left + 1}px`;
   }

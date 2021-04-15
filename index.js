486c91071ba00c9b4cadae4c68da6c81578adab9
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-15 09:50:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNumber, 10);
 }
 if(right < 180) {
-  dodger.style.right = `${left + 1}px`;
+  dodger.style.right = `${right + 1}px`;
 }
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
